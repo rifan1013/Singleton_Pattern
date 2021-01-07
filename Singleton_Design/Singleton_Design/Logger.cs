0299a2b2a2b7b1ac300b1d2519b7b3904d4df53b
rifan1013 <rifan7362@gmail.com>
2021-01-07 16:31:18 +0530

    singleton patern

---
+++ Singleton_Design/Singleton_Design/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPatterns._11Sigleton
+{
+   public class Logger
+    {
+        //private static Logger logger;
+        private static Logger instance;
+        private Logger()
+        {
+
+        }
+        public static Logger Instance
+        {
+
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Logger();
+                }
+                return instance;
+            }
+        }
+
+       // public static Logger GetInstance()
+       // {
+         //   if (logger == null)
+           // {
+             //   logger = new Logger();
+            //}
+            //return logger;
+        //}
+    }
+}
---
+++ Singleton_Design/Singleton_Design/TestLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPatterns._11Sigleton
+{
+    class TestLogger
+    {
+        static void Main()
+
+        {
+            //Logger obj1 = new Logger();
+            //Logger obj2 = new Logger();
+            // Logger obj1 = Logger.GetInstance();
+            // Logger obj2 = Logger.GetInstance();
+            Logger obj1 = Logger.Instance;
+            Logger obj2 = Logger.Instance;
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+        }
+    }
+}
